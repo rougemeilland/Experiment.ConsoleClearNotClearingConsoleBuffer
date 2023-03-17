69d263f6c928f5d5c957ccdb180e62d45b2a5004
rougemeilland <28302784+rougemeilland@users.noreply.github.com>
2023-03-17 16:05:33 +0900

    Initial build

---
+++ Experiment01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Experiment.ConsoleClearNotClearingConsoleBuffer.Experiment01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            for (var count = 0; count < 100; count++)
+                Console.WriteLine($"#{count}: This line should be cleared.");
+            Console.Clear();
+            Console.WriteLine("Check if all lines have been deleted by manipulating the console scroll bar.");
+            Console.WriteLine("And compare it with the result of running the cls command.");
+        }
+    }
+}
